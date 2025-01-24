c847177cb780df1377205b43ed036b7a8b00f0c8
SiddharthBITS <sengarsiddharth@yahoo.in>
2025-01-24 10:36:10 +0000

    Disabling pg_hint before atatuservar

--- test/JDBC/input/atatuservar-vu-verify.sql
+++ test/JDBC/input/atatuservar-vu-verify.sql
@@ -1,6 +1,10 @@
 -- simple variables in T-SQL batch
 set quoted_identifier off
 go
+SELECT set_config('babelfishpg_tsql.enable_pg_hint', 'off', false)
+go
+SET pg_hint_plan.debug_print TO off;
+go
 declare @@v int = 2 select @@v
 go
 declare @#v int = 2 select @#v
